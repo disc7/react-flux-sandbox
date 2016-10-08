fa85d927bdfc3ae5218da112d097b001636623ce
Charlie Abbott <charlie.abbott@gmail.com>
2016-10-08 09:14:43 +0100

    Making quick change to confirm github account

--- psadmin/src/components/common/header.js
+++ psadmin/src/components/common/header.js
@@ -15,7 +15,7 @@ var Header = React.createClass({
               <ul className="nav navbar-nav">
                 <li><Link to="app">Home</Link></li>
                 <li><Link to="about">About</Link></li>
-                <li><Link to="authors">Authors</Link></li>
+                <li><Link to="authors">Contact and Feedback</Link></li>
               </ul>
           </div>
         </nav>
